86c8bf999f0c152330f466f69a4564ce95307dc6
nuru <nurudiin222@gmail.com>
2023-03-26 08:44:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+const mainNode = document.querySelector("#main");
+mainNode.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "John Doe is the champion";
+
+
+document.body.appendChild(newHeader);
